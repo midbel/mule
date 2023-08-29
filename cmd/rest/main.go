0bd4028ec442a65044796f1b82bf86cbbfc7ee40
midbel <brun.nicolas21@gmail.com>
2023-08-29 21:19:29 +0200

    content type negociation

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"embed"
 	"encoding/json"
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"net/http"
@@ -16,23 +17,27 @@ import (
 )
 
 type Data struct {
-	Id      int       `json:"id"`
-	Name    string    `json:"label"`
-	Created time.Time `json:"date_created"`
-	Updated time.Time `json:"date_updated"`
+	XMLName xml.Name  `json:"-" xml:"data"`
+	Id      int       `json:"id" xml:"id,attr"`
+	Label   string    `json:"label" xml:"label"`
+	Created time.Time `json:"date_created" xml:"date-created"`
+	Updated time.Time `json:"date_updated" xml:"date-updated"`
 }
 
 func New(id int, name string) Data {
 	return Data{
 		Id:      id,
-		Name:    name,
+		Label:   name,
 		Created: time.Now(),
 		Updated: time.Now(),
 	}
 }
 
 func main() {
-	addr := flag.String("a", ":9001", "listening address")
+	var (
+		addr      = flag.String("a", ":9001", "listening address")
+		forceAuth = flag.Bool("x", false, "enable authentication")
+	)
 	flag.Parse()
 
 	set := []struct {
@@ -49,6 +54,9 @@ func main() {
 	}
 	for _, s := range set {
 		h, err := Prepare(s.Data)
+		if *forceAuth {
+			h = WithAuth(h)
+		}
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(2)
@@ -72,6 +80,29 @@ func Prepare(file string) (http.Handler, error) {
 	return h, nil
 }
 
+type Encoder interface {
+	Encode(interface{}) error
+}
+
+type authHandler struct {
+	http.Handler
+}
+
+func WithAuth(h http.Handler) http.Handler {
+	return authHandler{
+		Handler: h,
+	}
+}
+
+func (h authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	auth := r.Header.Get("authorization")
+	if auth == "" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	h.Handler.ServeHTTP(w, r)
+}
+
 type Limit struct {
 	Offset int
 	Count  int
@@ -86,12 +117,46 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions {
 		return
 	}
-	limit, err := h.getLimitFromRequest(r)
-	if err != nil {
+	data, err := h.getData(r)
+	switch {
+	case err != nil:
 		w.WriteHeader(http.StatusBadRequest)
+	case len(data) == 0:
+		w.WriteHeader(http.StatusNoContent)
+	default:
+	}
+	enc, err := h.getEncoder(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
 		return
 	}
-	json.NewEncoder(w).Encode(h.Data[limit.Offset : limit.Offset+limit.Count])
+	enc.Encode(data)
+}
+
+func (h handler) getEncoder(w http.ResponseWriter, r *http.Request) (Encoder, error) {
+	accept := r.Header.Values("accept")
+	if len(accept) == 0 {
+		return json.NewEncoder(w), nil
+	}
+	var list []WeightString
+	for _, a := range accept {
+		list = append(list, Weighted(a))
+	}
+	slices.SortFunc(list, func(i, j WeightString) int {
+		return j.Weight - i.Weight
+	})
+	for _, str := range list {
+		switch str.Value {
+		case "application/json":
+			w.Header().Set("content-type", str.Value)
+			return json.NewEncoder(w), nil
+		case "text/xml":
+			w.Header().Set("content-type", str.Value)
+			return xml.NewEncoder(w), nil
+		default:
+		}
+	}
+	return nil, fmt.Errorf("not acceptable")
 }
 
 func (h handler) setHeadersCORS(w http.ResponseWriter) {
@@ -105,7 +170,7 @@ func (h handler) setHeadersCORS(w http.ResponseWriter) {
 	w.Header().Add("Access-Control-Allow-Headers", "Origin")
 }
 
-func (h handler) getLimitFromRequest(r *http.Request) (Limit, error) {
+func (h handler) getData(r *http.Request) ([]Data, error) {
 	var (
 		lim    Limit
 		err    error
@@ -115,24 +180,24 @@ func (h handler) getLimitFromRequest(r *http.Request) (Limit, error) {
 		count  = q.Get("count")
 	)
 	if lim.Offset, err = strconv.Atoi(offset); err != nil && offset != "" {
-		return lim, err
+		return nil, err
 	}
 	if lim.Count, err = strconv.Atoi(count); err != nil && offset != "" {
-		return lim, err
+		return nil, err
 	}
 
 	if lim.Offset < 0 {
 		lim.Offset = size + lim.Offset
 	}
 	if lim.Offset < 0 || lim.Offset >= size {
-		return lim, fmt.Errorf("invalid offset")
+		return nil, fmt.Errorf("invalid offset")
 	}
 	if lim.Offset+lim.Count >= size {
 		lim.Count = size - lim.Offset
 	} else if lim.Count == 0 {
 		lim.Count = size
 	}
-	return lim, nil
+	return h.Data[lim.Offset : lim.Offset+lim.Count], nil
 }
 
 //go:embed resources/*txt
@@ -157,7 +222,30 @@ func Load(file string) ([]Data, error) {
 		list = append(list, New(i, line))
 	}
 	slices.SortFunc(list, func(i, j Data) int {
-		return strings.Compare(i.Name, j.Name)
+		return strings.Compare(i.Label, j.Label)
 	})
 	return list, scan.Err()
+}
+
+type WeightString struct {
+	Value  string
+	Weight int
+}
+
+const prefix = ";q="
+
+func Weighted(str string) WeightString {
+	var (
+		q = 100
+		x = strings.Index(str, prefix)
+	)
+	if x > 0 {
+		tmp, _ := strconv.ParseFloat(str[x+len(prefix):], 64)
+		q = int(tmp * 100)
+		str = str[:x]
+	}
+	return WeightString{
+		Value:  str,
+		Weight: q,
+	}
 }
