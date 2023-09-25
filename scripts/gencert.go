806692f877eea07c309b1a68b1ed5a50aaa7256e
midbel <brun.nicolas21@gmail.com>
2023-09-25 21:09:10 +0200

    generate client certificate

--- scripts/gencert.go
+++ scripts/gencert.go
@@ -3,15 +3,16 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
+  "crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
-  "encoding/pem"
+	"encoding/pem"
 	"flag"
 	"fmt"
 	"math/big"
-  "path/filepath"
-  "net"
+	"net"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -19,6 +20,7 @@ var now = time.Now()
 
 func main() {
 	var (
+		client = flag.Bool("c", false, "generate a client certificate")
 		server = flag.String("s", "localhost", "server name")
 		org    = flag.String("o", "midbel", "organization name")
 		dir    = flag.String("d", "", "certificate directory")
@@ -34,10 +36,10 @@ func main() {
 	}
 
 	priv, err := rsa.GenerateKey(rand.Reader, *bits)
-  if err != nil {
-    fmt.Fprintln(os.Stderr, "fail to generate key: %s", err)
-    os.Exit(2)
-  }
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "fail to generate key: %s", err)
+		os.Exit(2)
+	}
 
 	cert := x509.Certificate{
 		SerialNumber: getSerialNumber(),
@@ -47,69 +49,90 @@ func main() {
 		NotBefore: now,
 		NotAfter:  now.Add(*ttl),
 
-		KeyUsage:              getKeyUsage(*root),
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		KeyUsage:              getKeyUsage(*client, *root),
+		ExtKeyUsage:           []x509.ExtKeyUsage{getExtKeyUsage(*client)},
 		BasicConstraintsValid: true,
-    IsCA: *root,
+		IsCA:                  !*client && *root,
 	}
 
-  if ip := net.ParseIP(*server); ip == nil {
-    cert.DNSNames = append(cert.DNSNames, *server)
-  } else {
-    cert.IPAddresses = append(cert.IPAddresses, ip)
+  if !*client {
+    if ip := net.ParseIP(*server); ip == nil {
+      cert.DNSNames = append(cert.DNSNames, *server)
+      } else {
+        cert.IPAddresses = append(cert.IPAddresses, ip)
+      }
+  }
+  var parent *x509.Certificate
+  if *client {
+    parent, err = loadParentCertificate(flag.Arg(0))
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err)
+      os.Exit(2)
+    }
   }
 
-  if err := writeCertificate(&cert, priv, *dir); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(2)
-  }
+	if err := writeCertificate(&cert, parent, priv, *dir); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
 }
 
-func writeCertificate(cert *x509.Certificate, priv any, dir string) error {
-  key, ok := priv.(*rsa.PrivateKey)
-  if !ok {
-    return fmt.Errorf("unexpected private key type")
-  }
-  der, err := x509.CreateCertificate(rand.Reader, cert, cert, &key.PublicKey, priv)
+func loadParentCertificate(dir string) (*x509.Certificate, error) {
+  cert, err := tls.LoadX509KeyPair(filepath.Join(dir, "cert.pem"), filepath.Join(dir, "key.pem"))
   if err != nil {
-    return err
+    return nil, err
   }
-  if err := writePem(dir, der); err != nil {
-    return err
+  return cert.Leaf, nil
+}
+
+func writeCertificate(cert, root *x509.Certificate, priv any, dir string) error {
+	key, ok := priv.(*rsa.PrivateKey)
+	if !ok {
+		return fmt.Errorf("unexpected private key type")
+	}
+  if root == nil {
+    root = cert
   }
-  return nil
+	der, err := x509.CreateCertificate(rand.Reader, cert, root, &key.PublicKey, priv)
+	if err != nil {
+		return err
+	}
+	if err := writePem(dir, der); err != nil {
+		return err
+	}
+	return writeKey(dir, priv)
 }
 
 func writePem(dir string, der []byte) error {
-  w, err := os.Create(filepath.Join(dir, "cert.pem"))
-  if err != nil {
-    return err
-  }
-  defer w.Close()
+	w, err := os.Create(filepath.Join(dir, "cert.pem"))
+	if err != nil {
+		return err
+	}
+	defer w.Close()
 
-  block := pem.Block{
-    Type: "CERTIFICATE",
-    Bytes: der,
-  }
-  return pem.Encode(w, &block)
+	block := pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: der,
+	}
+	return pem.Encode(w, &block)
 }
 
 func writeKey(dir string, priv any) error {
-  w, err := os.Create(filepath.Join(dir, "key.pem"))
-  if err != nil {
-    return err
-  }
-  defer w.Close()
+	w, err := os.Create(filepath.Join(dir, "key.pem"))
+	if err != nil {
+		return err
+	}
+	defer w.Close()
 
-  raw, err := x509.MarshalPKCS8PrivateKey(priv)
-  if err != nil {
-    return err
-  }
-  block := pem.Block{
-    Type: "PRIVATE KEY",
-    Bytes: raw,
-  }
-  return pem.Encode(w, &block)
+	raw, err := x509.MarshalPKCS8PrivateKey(priv)
+	if err != nil {
+		return err
+	}
+	block := pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: raw,
+	}
+	return pem.Encode(w, &block)
 }
 
 func getSerialNumber() *big.Int {
@@ -118,10 +141,17 @@ func getSerialNumber() *big.Int {
 	return serial
 }
 
-func getKeyUsage(ca bool) x509.KeyUsage {
-	usage := x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment
-  if ca {
-    usage |= x509.KeyUsageCertSign
+func getExtKeyUsage(client bool) x509.ExtKeyUsage {
+  if client {
+    return x509.ExtKeyUsageClientAuth
   }
-  return usage
+  return x509.ExtKeyUsageServerAuth
+}
+
+func getKeyUsage(client, ca bool) x509.KeyUsage {
+	usage := x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment
+	if ca && !client {
+		usage |= x509.KeyUsageCertSign
+	}
+	return usage
 }
